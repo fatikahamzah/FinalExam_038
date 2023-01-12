9c58f184488ccc921d6a84244647f4174e804dd0
fatikahamzah <fatikahmzptr@gmail.com>
2023-01-12 19:37:28 +0700

    latihan

---
+++ Documents/NetBeansProjects/javapratikum/src/test/java/tugaspratikum/javapratikum/JavapratikumApplicationTests.java
@@ -0,0 +1,13 @@
+package tugaspratikum.javapratikum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavapratikumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
